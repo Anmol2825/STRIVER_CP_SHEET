c8da3d06132e450109c519e4c64839239624dbbc
Anmol Agarwal <mailanmolat@gmail.com>
2022-07-22 11:43:22 +0530

    Tram

---
+++ Implementation/13.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector<int>
+#define pii pair<int, int>
+#define vii vector<pii>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define ff first
+#define ss second
+#define setBits(x) builtin_popcount(x)
+int main()
+{
+    int n;
+    cin >> n;
+    int ans = 0;
+    int count = 0;
+    vector<int> a(n);
+    vector<int> b(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i] >> b[i];
+        count += (b[i] - a[i]);
+        ans = max(ans, count);
+    }
+    cout << ans << endl;
+    return 0;
+}
