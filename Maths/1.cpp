aa0bbe59f13d34e13743d686ff60ca7f0ed41aab
Anmol Agarwal <mailanmolat@gmail.com>
2022-07-22 14:48:16 +0530

    Ichihime and Triangle

---
+++ Maths/1.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define vi vector<int>
+#define pii pair<int, int>
+#define vii vector<pii>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define ff first
+#define ss second
+#define setBits(x) builtin_popcount(x)
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+        cout << a << " " << c << " " << c << endl;
+    }
+
+    return 0;
+}
